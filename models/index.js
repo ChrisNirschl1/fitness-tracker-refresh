e3647941123dcd9f265424943c19d9ba411fda90
Christian Nirschl <christiannirschl6@gmail.com>
2021-12-07 23:11:54 -0800

    n/a

---
+++ models/index.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: new Date(),
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "exercise type"
+            },
+            name: { 
+                type: String,
+                trim: true,
+                 required: "name of exercise"
+            },
+            duration: {
+                type: Number,
+                required: "exercise duration"
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ],
+})
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
